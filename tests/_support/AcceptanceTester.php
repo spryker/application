1007392722809fc17c8c7bcd424bc96f384d3232
René Klatt <rene.klatt@spryker.com>
2016-07-12 06:13:52 +0000

    Added Braintree Acceptance tests Added Codeception modules in several bundles Fixed wrong const usage in Braintree Updated braintree/braintree_lib to 3.15

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -13,15 +13,13 @@ namespace Application;
  * @method void am($role)
  * @method void lookForwardTo($achieveValue)
  * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = null)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
  *
  * @SuppressWarnings(PHPMD)
 */
-class UnitTester extends \Codeception\Actor
+class AcceptanceTester extends \Codeception\Actor
 {
-    use _generated\UnitTesterActions;
 
-   /**
-    * Define custom actions here
-    */
+    use _generated\AcceptanceTesterActions;
+
 }
